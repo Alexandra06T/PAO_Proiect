887511f23fe5e7c873d423caf9d333712dc2beed
Alexandra06T <115272509+Alexandra06T@users.noreply.github.com>
2024-04-09 13:53:44 +0300

    split main

--- src/Application.java
+++ src/Application.java
@@ -10,6 +10,7 @@ public class Application {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         BookService bookService = new BookService();
         BranchLibraryService branchLibraryService = new BranchLibraryService();
         CategoryService categoryService = new CategoryService();
@@ -19,6 +20,62 @@ public class Application {
         ReservationService reservationService = new ReservationService();
         TransactionService transactionService = new TransactionService();
 
+        ObjectsExamples();
+
+
+        while (true){
+
+            menuTarget();
+            String target = scanner.nextLine().toLowerCase();
+            switch (target) {
+                case "category":
+                    LibraryService.manageCategories(scanner, categoryService);
+                    break;
+                case "book":
+                    LibraryService.manageBooks(scanner, bookService);
+                    break;
+                case "book copy":
+                    LibraryService.manageBookCopies(scanner, bookCopyService);
+                    break;
+                case "library member":
+                    LibraryService.manageLibraryMembers(scanner, libraryMemberService);
+                    break;
+                case "branch library":
+                    LibraryService.manageBranchLibraries(scanner, branchLibraryService);
+                    break;
+                case "location":
+                    LibraryService.manageLocations(scanner, locationService);
+                    break;
+                case "reservation":
+                    LibraryService.manageReservations(scanner, reservationService);
+                    break;
+                case "transaction":
+                    LibraryService.manageTransactions(scanner, transactionService);
+                    break;
+                case "quit":
+                    System.out.println("Exiting");
+                    return;
+                default:
+                    System.out.println("The chosen target couldn't be recognized.");
+            }
+        }
+    }
+
+    private static void menuTarget() {
+        System.out.println("Choose the target of your operation:");
+        System.out.println("Category");
+        System.out.println("Book");
+        System.out.println("Book Copy");
+        System.out.println("Library Member");
+        System.out.println("Location");
+        System.out.println("Branch Library");
+        System.out.println("Reservation");
+        System.out.println("Transaction");
+        System.out.println("quit");
+        System.out.println("Enter target:");
+    }
+
+    private static void ObjectsExamples() {
         //mini baza de date pentru testare
         BranchLibraryRepositoryService branchLibraryRepositoryService = new BranchLibraryRepositoryService();
         BranchLibrary MatematicaInformatica = new BranchLibrary("Facultatea de Matematica si Informatica", "Strada Academiei, nr 14");
@@ -104,236 +161,6 @@ public class Application {
         transactionRepositoryService.addTransaction(checkIn1);
         transactionRepositoryService.addTransaction(checkIn2);
         transactionRepositoryService.addTransaction(checkIn3);
-
-        while (true){
-
-            menuTarget();
-            String target = scanner.nextLine().toLowerCase();
-            switch (target) {
-                case "category":
-                    menuCRUD();
-
-                    String command = scanner.nextLine().toLowerCase();
-                    switch (command){
-                        case "create":
-                            categoryService.create(scanner);
-                            break;
-                        case "read":
-                            categoryService.read(scanner);
-                            break;
-                        case "delete":
-                            categoryService.delete(scanner);
-                            break;
-                        case "update":
-                            categoryService.update(scanner);
-                            break;
-                        case "quit":
-                            System.out.println("Exiting");
-                            return;
-                        default:
-                            System.out.println("The chosen command couldn't be recognized");
-                    }
-                    break;
-                case "book":
-                    menuCRUD();
-
-                    command = scanner.nextLine().toLowerCase();
-                    switch (command){
-                        case "create":
-                            bookService.create(scanner);
-                            break;
-                        case "read":
-                            bookService.read(scanner);
-                            break;
-                        case "delete":
-                            bookService.delete(scanner);
-                            break;
-                        case "update":
-                            bookService.update(scanner);
-                            break;
-                        case "quit":
-                            System.out.println("Exiting");
-                            return;
-                        default:
-                            System.out.println("The chosen command couldn't be recognized");
-                    }
-                    break;
-                case "book copy":
-                    menuCRUD();
-
-                    command = scanner.nextLine().toLowerCase();
-                    switch (command){
-                        case "create":
-                            bookCopyService.create(scanner);
-                            break;
-                        case "read":
-                            bookCopyService.read(scanner);
-                            break;
-                        case "delete":
-                            bookCopyService.delete(scanner);
-                            break;
-                        case "update":
-                            bookCopyService.update(scanner);
-                            break;
-                        case "quit":
-                            System.out.println("Exiting");
-                            return;
-                        default:
-                            System.out.println("The chosen command couldn't be recognized");
-                    }
-                    break;
-                case "library member":
-                    menuCRUD();
-
-                    command = scanner.nextLine().toLowerCase();
-                    switch (command){
-                        case "create":
-                            libraryMemberService.create(scanner);
-                            break;
-                        case "read":
-                            libraryMemberService.read(scanner);
-                            break;
-                        case "delete":
-                            libraryMemberService.delete(scanner);
-                            break;
-                        case "update":
-                            libraryMemberService.update(scanner);
-                            break;
-                        case "quit":
-                            System.out.println("Exiting");
-                            return;
-                        default:
-                            System.out.println("The chosen command couldn't be recognized");
-                    }
-                    break;
-                case "branch library":
-                    menuCRUD();
-
-                    command = scanner.nextLine().toLowerCase();
-                    switch (command){
-                        case "create":
-                            branchLibraryService.create(scanner);
-                            break;
-                        case "read":
-                            branchLibraryService.read(scanner);
-                            break;
-                        case "delete":
-                            branchLibraryService.delete(scanner);
-                            break;
-                        case "update":
-                            branchLibraryService.update(scanner);
-                            break;
-                        case "quit":
-                            System.out.println("Exiting");
-                            return;
-                        default:
-                            System.out.println("The chosen command couldn't be recognized");
-                    }
-                    break;
-                case "location":
-                    menuCRUD();
-
-                    command = scanner.nextLine().toLowerCase();
-                    switch (command){
-                        case "create":
-                            locationService.create(scanner);
-                            break;
-                        case "read":
-                            locationService.read(scanner);
-                            break;
-                        case "delete":
-                            locationService.delete(scanner);
-                            break;
-                        case "update":
-                            locationService.update(scanner);
-                            break;
-                        case "quit":
-                            System.out.println("Exiting");
-                            return;
-                        default:
-                            System.out.println("The chosen command couldn't be recognized");
-                    }
-                    break;
-                case "reservation":
-
-                    menuCRUD();
-
-                    command = scanner.nextLine().toLowerCase();
-                    switch (command){
-                        case "create":
-                            reservationService.create(scanner);
-                            break;
-                        case "read":
-                            reservationService.read(scanner);
-                            break;
-                        case "delete":
-                            reservationService.delete(scanner);
-                            break;
-                        case "update":
-                            reservationService.update(scanner);
-                            break;
-                        case "quit":
-                            System.out.println("Exiting");
-                            return;
-                        default:
-                            System.out.println("The chosen command couldn't be recognized");
-                    }
-                    break;
-                case "transaction":
-                    menuCRUD();
-
-                    command = scanner.nextLine().toLowerCase();
-                    switch (command){
-                        case "create":
-                            transactionService.create(scanner);
-                            break;
-                        case "read":
-                            transactionService.read(scanner);
-                            break;
-                        case "delete":
-                            transactionService.delete(scanner);
-                            break;
-                        case "update":
-                            transactionService.update(scanner);
-                            break;
-                        case "quit":
-                            System.out.println("Exiting");
-                            return;
-                        default:
-                            System.out.println("The chosen command couldn't be recognized");
-                    }
-                    break;
-                case "quit":
-                    System.out.println("Exiting");
-                    return;
-                default:
-                    System.out.println("The chosen target couldn't be recognized.");
-            }
-        }
-    }
-
-    private static void menuCRUD() {
-        System.out.println("Choose the operation you want to perform:");
-        System.out.println("create");
-        System.out.println("read");
-        System.out.println("update");
-        System.out.println("delete");
-        System.out.println("quit");
-        System.out.println("Enter command:");
-    }
-
-    private static void menuTarget() {
-        System.out.println("Choose the target of your operation:");
-        System.out.println("Category");
-        System.out.println("Book");
-        System.out.println("Book Copy");
-        System.out.println("Library Member");
-        System.out.println("Location");
-        System.out.println("Branch Library");
-        System.out.println("Reservation");
-        System.out.println("Transaction");
-        System.out.println("quit");
-        System.out.println("Enter target:");
     }
 
 
---
+++ src/service/LibraryService.java
@@ -0,0 +1,216 @@
+package service;
+
+import java.util.Scanner;
+
+public class LibraryService {
+
+    private static void menuCRUD() {
+        System.out.println("Choose the operation you want to perform:");
+        System.out.println("create");
+        System.out.println("read");
+        System.out.println("update");
+        System.out.println("delete");
+        System.out.println("quit");
+        System.out.println("Enter command:");
+    }
+
+    public static void manageCategories(Scanner scanner, CategoryService categoryService) {
+        menuCRUD();
+
+        String command = scanner.nextLine().toLowerCase();
+        switch (command){
+            case "create":
+                categoryService.create(scanner);
+                break;
+            case "read":
+                categoryService.read(scanner);
+                break;
+            case "delete":
+                categoryService.delete(scanner);
+                break;
+            case "update":
+                categoryService.update(scanner);
+                break;
+            case "quit":
+                System.out.println("Exiting");
+                return;
+            default:
+                System.out.println("The chosen command couldn't be recognized");
+        }
+    }
+
+    public static void manageBooks(Scanner scanner, BookService bookService) {
+        menuCRUD();
+
+        String command = scanner.nextLine().toLowerCase();
+        switch (command){
+            case "create":
+                bookService.create(scanner);
+                break;
+            case "read":
+                bookService.read(scanner);
+                break;
+            case "delete":
+                bookService.delete(scanner);
+                break;
+            case "update":
+                bookService.update(scanner);
+                break;
+            case "quit":
+                System.out.println("Exiting");
+                return;
+            default:
+                System.out.println("The chosen command couldn't be recognized");
+        }
+    }
+
+    public static void manageBookCopies(Scanner scanner, BookCopyService bookCopyService) {
+        menuCRUD();
+
+        String command = scanner.nextLine().toLowerCase();
+        switch (command){
+            case "create":
+                bookCopyService.create(scanner);
+                break;
+            case "read":
+                bookCopyService.read(scanner);
+                break;
+            case "delete":
+                bookCopyService.delete(scanner);
+                break;
+            case "update":
+                bookCopyService.update(scanner);
+                break;
+            case "quit":
+                System.out.println("Exiting");
+                return;
+            default:
+                System.out.println("The chosen command couldn't be recognized");
+        }
+    }
+
+    public static void manageLibraryMembers(Scanner scanner, LibraryMemberService libraryMemberService) {
+        menuCRUD();
+
+        String command = scanner.nextLine().toLowerCase();
+        switch (command){
+            case "create":
+                libraryMemberService.create(scanner);
+                break;
+            case "read":
+                libraryMemberService.read(scanner);
+                break;
+            case "delete":
+                libraryMemberService.delete(scanner);
+                break;
+            case "update":
+                libraryMemberService.update(scanner);
+                break;
+            case "quit":
+                System.out.println("Exiting");
+                return;
+            default:
+                System.out.println("The chosen command couldn't be recognized");
+        }
+    }
+
+    public static void manageBranchLibraries(Scanner scanner, BranchLibraryService branchLibraryService) {
+        menuCRUD();
+
+        String command = scanner.nextLine().toLowerCase();
+        switch (command){
+            case "create":
+                branchLibraryService.create(scanner);
+                break;
+            case "read":
+                branchLibraryService.read(scanner);
+                break;
+            case "delete":
+                branchLibraryService.delete(scanner);
+                break;
+            case "update":
+                branchLibraryService.update(scanner);
+                break;
+            case "quit":
+                System.out.println("Exiting");
+                return;
+            default:
+                System.out.println("The chosen command couldn't be recognized");
+        }
+    }
+
+    public static void manageLocations(Scanner scanner, LocationService locationService) {
+        menuCRUD();
+
+        String command = scanner.nextLine().toLowerCase();
+        switch (command){
+            case "create":
+                locationService.create(scanner);
+                break;
+            case "read":
+                locationService.read(scanner);
+                break;
+            case "delete":
+                locationService.delete(scanner);
+                break;
+            case "update":
+                locationService.update(scanner);
+                break;
+            case "quit":
+                System.out.println("Exiting");
+                return;
+            default:
+                System.out.println("The chosen command couldn't be recognized");
+        }
+    }
+
+    public static void manageReservations(Scanner scanner, ReservationService reservationService) {
+        menuCRUD();
+
+        String command = scanner.nextLine().toLowerCase();
+        switch (command){
+            case "create":
+                reservationService.create(scanner);
+                break;
+            case "read":
+                reservationService.read(scanner);
+                break;
+            case "delete":
+                reservationService.delete(scanner);
+                break;
+            case "update":
+                reservationService.update(scanner);
+                break;
+            case "quit":
+                System.out.println("Exiting");
+                return;
+            default:
+                System.out.println("The chosen command couldn't be recognized");
+        }
+    }
+
+    public static void manageTransactions(Scanner scanner, TransactionService transactionService) {
+        menuCRUD();
+
+        String command = scanner.nextLine().toLowerCase();
+        switch (command){
+            case "create":
+                transactionService.create(scanner);
+                break;
+            case "read":
+                transactionService.read(scanner);
+                break;
+            case "delete":
+                transactionService.delete(scanner);
+                break;
+            case "update":
+                transactionService.update(scanner);
+                break;
+            case "quit":
+                System.out.println("Exiting");
+                return;
+            default:
+                System.out.println("The chosen command couldn't be recognized");
+        }
+    }
+}
